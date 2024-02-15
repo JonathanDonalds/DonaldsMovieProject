020fe3e4741a51ad4152b1cb4547ebbf71e1b964
BT_2N2_05 <sample@bths.edu>
2024-02-15 14:00:29 -0500

    2:00

--- src/MovieCollection.java
+++ src/MovieCollection.java
@@ -5,21 +5,26 @@ import java.util.Scanner;
 
 public class MovieCollection {
     private static ArrayList<Movie> movies;
+    private static ArrayList<String> wordList;
     public MovieCollection() {
         movies = new ArrayList<>();
+        wordList = new ArrayList<>();
         start();
     }
 
     public static void importCSV() {
         try {
-            Scanner input = new Scanner(new File("src\\words.txt"));
+            Scanner input = new Scanner(new File("src\\movies_data.csv"));
             while (input.hasNext()) {
                 String word = input.next();
                 wordList.add(word);
+                /*Movie movie = input.next();
+                movies.add(movie);*/
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+        System.out.println(wordList);
     }
 
 
