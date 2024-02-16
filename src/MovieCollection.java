4a3dad899bad7f413123d7443a384d54071575fd
Jonathan <jdonalds100@gmail.com>
2024-02-16 13:29:04 -0500

    1:28

--- src/MovieCollection.java
+++ src/MovieCollection.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MovieCollection {
@@ -13,18 +14,25 @@ public class MovieCollection {
     }
 
     public static void importCSV() {
+        ArrayList<String> title = new ArrayList<>();
         try {
-            Scanner input = new Scanner(new File("src\\movies_data.csv"));
+            Scanner input = new Scanner(new File("src//movies_data.csv"));
+            input.nextLine();
             while (input.hasNext()) {
-                String word = input.next();
+                String word = input.nextLine();
                 wordList.add(word);
                 /*Movie movie = input.next();
                 movies.add(movie);*/
             }
+            /*title = new ArrayList<>();
+            for (int i = 0; i < wordList.size(); i++) {
+                title.add(wordList.get(i).substring(0, wordList.get(i).indexOf(",")));
+            }*/
+
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
-        System.out.println(wordList);
+        System.out.println(title);
     }
 
 
