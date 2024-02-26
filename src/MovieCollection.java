98074050666556a2dec88fd418a788d51f6a2061
Jonathan <jdonalds100@gmail.com>
2024-02-25 21:56:54 -0500

    9:57

--- src/MovieCollection.java
+++ src/MovieCollection.java
@@ -1,18 +1,17 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MovieCollection {
     private static ArrayList<Movie> movies;
-    private static ArrayList<String> titles;
     private static Scanner scan;
 
 
     public MovieCollection() {
         movies = new ArrayList<>();
         scan = new Scanner(System.in);
-        titles = new ArrayList<>();
         start();
     }
 
@@ -64,6 +63,7 @@ public class MovieCollection {
     }
 
     public void searchTitles() {
+        ArrayList<String> titles = new ArrayList<>();
         String term;
         System.out.print("Enter a title search term: ");
         term = scan.nextLine();
@@ -85,6 +85,12 @@ public class MovieCollection {
             int num = scan.nextInt();
             scan.nextLine();
             System.out.println();
+            System.out.println(titles.get(num - 1));
+            for (Movie movie : movies) {
+                if (movie.getTitle().equals(titles.get(num - 1))) {
+                    System.out.println(movie.getInfo());
+                }
+            }
         }
     }
 
@@ -100,6 +106,51 @@ public class MovieCollection {
     }
 
     public void searchCast() {
-
+        ArrayList<String> cast = new ArrayList<>();
+        String term;
+        System.out.print("Enter a person to search for (first and last name): ");
+        term = scan.nextLine();
+        for (Movie movie : movies) {
+            String str = movie.getCast();
+            String[] array = str.split("\\|");
+            for (String member : array) {
+                if ((member.toLowerCase().contains(term.toLowerCase()) || member.toUpperCase().contains(term.toUpperCase())) && !cast.contains(member)) {
+                    cast.add(member);
+                }
+            }
+        }
+        if (cast.isEmpty()) {
+            System.out.println();
+            System.out.println("No results match your search");
+        } else {
+            sort(cast);
+            for (int i = 0; i < cast.size(); i++) {
+                System.out.println((i + 1) + ". " + cast.get(i));
+            }
+            System.out.println("Which would you like to see all movies for?");
+            System.out.print("Enter number: ");
+            int num = scan.nextInt();
+            scan.nextLine();
+            ArrayList<String> list = new ArrayList<>();
+            for (Movie movie : movies) {
+                if (movie.getCast().contains(cast.get(num - 1))) {
+                    list.add(movie.getTitle());
+                }
+            }
+            sort(list);
+            for (int i = 0; i < list.size(); i++) {
+                System.out.println((i + 1) + ". " + list.get(i));
+            }
+            System.out.println("Which movie would you like to learn more about?");
+            System.out.print("Enter number: ");
+            int number = scan.nextInt();
+            scan.nextLine();
+            System.out.println();
+            for (Movie movie : movies) {
+                if (movie.getTitle().equals(list.get(number - 1))) {
+                    System.out.println(movie.getInfo());
+                }
+            }
+        }
     }
 }
